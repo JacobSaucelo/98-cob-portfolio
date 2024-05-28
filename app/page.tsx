0fec2e6df15cb28ef4dae8d7be41949451d4c45a
Saucelo <jacobsaucelo32@gmail.com>
2024-05-28 11:11:34 +0800

    added experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import EssentialsNavbar from "@/components/Essentials.Navbar";
 import styles from "./page.module.css";
-import SectionTechStack from "@/components/sections/Section.TechStack";
+import SectionTechStack from "@/components/sections/TechStackSection/Section.TechStack";
+import SectionExperience from "@/components/sections/Experience/Section.Experience";
 
 export default function Home() {
   return (
     <main className={styles.HomePageContainer}>
       <EssentialsNavbar />
       <SectionTechStack />
+      <SectionExperience />
     </main>
   );
 }
---
+++ components/sections/Experience/Section.Experience.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SectionExperience = () => {
+  return <section>asdf</section>;
+};
+
+export default SectionExperience;
