44b125b7ca4078abbc95fa3ffbc9bcd9f534d668
Saucelo <jacobsaucelo32@gmail.com>
2024-05-29 15:33:12 +0800

    added hero pop up

--- components/sections/Hero/Section.Hero.tsx
+++ components/sections/Hero/Section.Hero.tsx
@@ -38,6 +38,22 @@ const SectionHero = () => {
           <p className={styles.SectionHeroDetailsSubTitle}>the tab content</p>
         </aside>
       </div>
+
+      <section>
+        <div className="window">
+          <div className="title-bar">
+            <div className="title-bar-text">A Window With Stuff In It</div>
+            <div className="title-bar-controls">
+              <button aria-label="Minimize"></button>
+              <button aria-label="Maximize"></button>
+              <button aria-label="Close"></button>
+            </div>
+          </div>
+          <div className="window-body">
+            <p>There&apos;s so much room for activities!</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
