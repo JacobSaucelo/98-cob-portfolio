1ac86f5cdc1762ea59904099c2963f2870fefab3
Saucelo <jacobsaucelo32@gmail.com>
2024-05-30 09:09:24 +0800

    added card with the banner fixed

--- components/sections/NotFound/Section.NotFound.tsx
+++ components/sections/NotFound/Section.NotFound.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import styles from "./Section.NotFound.module.css";
+import Image from "next/image";
 
 const SectionNotFound = () => {
   return (
     <section className={styles.SectionNotFoundContainer}>
-      SectionNotFound
+      <div className={`${styles.SectionWindow} window`}>
+        <div className={`${styles.WindowTitleBar} title-bar`}>
+          {/* style={{ backgroundColor: "rgba(0,0,168,1) !important" }} */}
+          <div className="title-bar-text">Page Not Found</div>
+          <div className="title-bar-controls"></div>
+        </div>
+        <div className={`${styles.WindowBody} window-body`}>
+          <aside className={styles.WindowBodyImageContainer}>
+            <Image
+              src="/assets/notFoundBannerFixed.png"
+              height={260}
+              width={120}
+              alt="Page not found"
+              className={styles.WindowBodyImage}
+            />
+          </aside>
+          <aside>
+            <p>Page Not Found : (</p>
+            <p>There&apos;s so much room for activities!</p>
+          </aside>
+        </div>
+      </div>
     </section>
   );
 };
